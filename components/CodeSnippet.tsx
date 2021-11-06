f364189a9b1b35f5259896a72e636e16815910db
gabo <gabriel.gaboardi13@gmail.com>
2021-11-05 22:23:51 -0300

    added copy to clipboard for snippets

--- components/CodeSnippet.tsx
+++ components/CodeSnippet.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement, ReactNode } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "../styles/highlight";
+import styles from "../styles/CodeSnippet.module.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 interface Props {
   children: ReactNode;
@@ -15,7 +17,12 @@ export default function CodeSnippet({
 }: Props): ReactElement {
   const codeLanguage = /language-(\w+)/.exec(className);
   return (
-    <div>
+    <div className={styles.codeContainer}>
+      <div className={styles.copyButton}>
+        <CopyToClipboard text={String(children).replace(/\n$/, "")}>
+          <span className="material-icons">content_copy</span>
+        </CopyToClipboard>
+      </div>
       <SyntaxHighlighter
         language={codeLanguage ? codeLanguage[1] : "text"}
         style={style}
