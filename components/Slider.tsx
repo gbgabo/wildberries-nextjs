d8bff07863a33c5e32a5fd748e43a7c14695f5af
gabo <gabriel.gaboardi13@gmail.com>
2021-12-30 20:58:23 -0300

    added html+css only slider

---
+++ components/Slider.tsx
@@ -0,0 +1,133 @@
+import React, { Fragment, ReactElement } from "react";
+import styles from "../styles/Slider.module.css";
+
+interface Props {
+  ports: string[];
+}
+
+export default function Slider({ ports }: Props): ReactElement {
+  return (
+    <>
+      <div className={styles["slider-wrapper"]}>
+        <div className={styles.slider}>
+          {ports.map((port, index) => {
+            return (
+              <Fragment key={port}>
+                <input
+                  type="radio"
+                  name="slider"
+                  className={styles.trigger}
+                  id={port}
+                  defaultChecked={index === 0}
+                />
+                <div className={styles.slide}>
+                  <figure className={styles["slide-figure"]}>
+                    <img
+                      className={styles["slide-img"]}
+                      src={`/img/screenshots/${port}.png`}
+                    />
+                    <figcaption className={styles["slide-caption"]}>
+                      <p>{port}</p>
+                    </figcaption>
+                  </figure>
+                </div>
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
+      <ul className={styles["slider-nav"]}>
+        {ports.map((port) => {
+          return (
+            <li key={port} className={styles["slider-nav__item"]}>
+              <label
+                className={styles["slider-nav__label"]}
+                htmlFor={port}
+              ></label>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+/*
+<div className={styles["slider-wrapper"]}>
+        <div className={styles.slider}>
+          <input
+            type="radio"
+            name="slider"
+            className={styles.trigger}
+            id="one"
+            defaultChecked
+          />
+          <div className={styles.slide}>
+            <figure className={styles["slide-figure"]}>
+              <img
+                className={styles["slide-img"]}
+                src="/img/screenshots/dmenu.png"
+              />
+              <figcaption className={styles["slide-caption"]}>
+                <p>dmenu</p>
+              </figcaption>
+            </figure>
+          </div>
+          <input
+            type="radio"
+            name="slider"
+            className={styles.trigger}
+            id="two"
+          />
+          <div className={styles.slide}>
+            <figure className={styles["slide-figure"]}>
+              <img
+                className={styles["slide-img"]}
+                src="/img/screenshots/gtk.png"
+              />
+              <figcaption className={styles["slide-caption"]}>
+                <p>gtk</p>
+              </figcaption>
+            </figure>
+          </div>
+          <input
+            type="radio"
+            name="slider"
+            className={styles.trigger}
+            id="three"
+          />
+          <div className={styles.slide}>
+            <figure className={styles["slide-figure"]}>
+              <img
+                className={styles["slide-img"]}
+                src="/img/screenshots/duckduckgo.png"
+              />
+              <figcaption className={styles["slide-caption"]}>
+                <p>duckduckgo</p>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <ul className={styles["slider-nav"]}>
+          <li className={styles["slider-nav__item"]}>
+            <label
+              className={styles["slider-nav__label"]}
+              htmlFor="one"
+            ></label>
+          </li>
+          <li className={styles["slider-nav__item"]}>
+            <label
+              className={styles["slider-nav__label"]}
+              htmlFor="two"
+            ></label>
+          </li>
+          <li className={styles["slider-nav__item"]}>
+            <label
+              className={styles["slider-nav__label"]}
+              htmlFor="three"
+            ></label>
+          </li>
+        </ul>
+      </div>
+
+*/
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Slider from "./Slider";
 
-export { Header, Footer };
+export { Header, Footer, Slider };
