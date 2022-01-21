d6ebd6c91d75324727d2e3f5fa6a95922deeca2e
gabo <gabriel.gaboardi13@gmail.com>
2022-01-21 00:33:01 -0300

    Updated footer to contain fab

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,24 +3,26 @@ import styles from "../styles/Footer.module.css";
 
 export default function Footer(): ReactElement {
   return (
-    <footer className={styles.footer}>
-      <p>
-        Made with ♥ by{" "}
-        <a href="https://github.com/gbgabo" rel="noreferrer" target="_blank">
-          @gbgabo
-        </a>
-      </p>
+    <div className={styles.background}>
+      <footer className={styles.footer}>
+        <p>
+          Made with ♥ by{" "}
+          <a href="https://github.com/gbgabo" rel="noreferrer" target="_blank">
+            @gbgabo
+          </a>
+        </p>
 
-      <p>
-        View the project on{" "}
-        <a
-          href="https://github.com/gbgabo/wildberries"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Github
-        </a>
-      </p>
-    </footer>
+        <p>
+          View the project on{" "}
+          <a
+            href="https://github.com/gbgabo/wildberries"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Github
+          </a>
+        </p>
+      </footer>
+    </div>
   );
 }
