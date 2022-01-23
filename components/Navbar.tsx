587fa24445bc30620201ed3898acb53226306c5c
gabo <gabriel.gaboardi13@gmail.com>
2022-01-23 20:39:05 -0300

    Added link and hover property on logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,18 @@
 import React, { ReactElement } from "react";
 import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 interface Props {}
 
 export default function Navbar({}: Props): ReactElement {
   return (
-    <div className={styles.container}>
-      <img src="/img/ui/wb.png" />
-      <h1>Wildberries</h1>
+    <div className={styles.bar}>
+      <Link href="/">
+        <a className={styles.logo}>
+          <img src="/img/ui/wb.png" />
+          <h1>Wildberries</h1>
+        </a>
+      </Link>
     </div>
   );
 }
