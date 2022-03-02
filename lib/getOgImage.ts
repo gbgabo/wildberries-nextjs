86c1b369a38eb57b12b0b4840b7bd10f4c35f0a1
gabo <gabriel.gaboardi13@gmail.com>
2022-03-01 21:53:23 -0300

    updated ogimage base url

--- lib/getOgImage.ts
+++ lib/getOgImage.ts
@@ -7,11 +7,11 @@ import { chromium } from "playwright-chromium";
 
 export default async function getOgImage(
   path: string,
-  baseUrl = "localhost:3000/"
+  baseUrl = "https://wildberries.style/"
 ) {
-  // if (process.env.NODE_ENV === "development") {
-  //   return "og image will be generated in production";
-  // }
+  if (process.env.NODE_ENV === "development") {
+    return "og image will be generated in production";
+  }
 
   const url = `${baseUrl}${path}`;
   const hash = createHash("md5").update(url).digest("hex");
