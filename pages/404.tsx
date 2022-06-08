c0a0b8ea7a6817d40f9b5c52f1807b4c55c7e146
gabo <gabriel.gaboardi13@gmail.com>
2022-06-08 18:15:12 -0300

    added 40 metatags

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,25 +1,40 @@
 import React, { ReactElement } from "react";
 import Link from "next/link";
+import Head from "next/head";
 import styles from "../styles/FourOhFour.module.css";
 
 export default function FourOhFour(): ReactElement {
   return (
-    <div className={styles.container}>
-      <h1>404</h1>
-      <h2>Nothing fruitful to find here</h2>
-      <div className={styles.options}>
-        <Link href="/">
-          <a>
-            Available ports <img src="/icons/brush.svg" />
-          </a>
-        </Link>
-        <Link href="/about">
-          <a>
-            About Wildberries
-            <img src="/icons/pallete.svg" />
-          </a>
-        </Link>
+    <>
+      <Head>
+        <title>404: Nothing fruitful here!</title>
+        <meta property="og:title" content="404" key="ogtitle" />
+
+        <meta name="description" content="Nothing fruitful here!" />
+        <meta
+          property="og:description"
+          content="Nothing fruitful here!"
+          key="ogdesc"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <h1>404</h1>
+        <h2>Nothing fruitful to find here</h2>
+        <div className={styles.options}>
+          <Link href="/">
+            <a>
+              Available ports <img src="/icons/brush.svg" />
+            </a>
+          </Link>
+          <Link href="/about">
+            <a>
+              About Wildberries
+              <img src="/icons/pallete.svg" />
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
