128a8599b3cc0f7ffad57dab5da613e225314ed5
gabo <gabriel.gaboardi13@gmail.com>
2022-08-13 15:11:28 -0300

    updated 404 styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -19,20 +19,22 @@ export default function FourOhFour(): ReactElement {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.container}>
-        <h1>404</h1>
-        <h2>Nothing fruitful to find here</h2>
-        <div className={styles.options}>
-          <Link href="/">
-            <a>
-              Available ports <img src="/icons/brush.svg" />
-            </a>
-          </Link>
-          <Link href="/about">
-            <a>
-              About Wildberries
-              <img src="/icons/pallete.svg" />
-            </a>
-          </Link>
+        <div className={styles.section}>
+          <h1>404</h1>
+          <h2>Nothing fruitful to find here</h2>
+          <div className={styles.options}>
+            <Link href="/">
+              <a>
+                Available ports <img src="/icons/brush.svg" />
+              </a>
+            </Link>
+            <Link href="/about">
+              <a>
+                About Wildberries
+                <img src="/icons/pallete.svg" />
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </>
