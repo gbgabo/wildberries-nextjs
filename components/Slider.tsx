8ec606b190f077d3e7b23d4dfff808fa0cd9826f
gabo <gabriel.gaboardi13@gmail.com>
2022-10-15 22:53:38 -0300

    fixed keys warning

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -16,6 +16,7 @@ export default function Slider({ slides }: Props): ReactElement {
       {slides.map((slide, index) => {
         return (
           <input
+            key={index}
             type="radio"
             defaultChecked={index === 0}
             name="slides"
@@ -24,9 +25,9 @@ export default function Slider({ slides }: Props): ReactElement {
         );
       })}
       <ul>
-        {slides.map((slide) => {
+        {slides.map((slide, index) => {
           return (
-            <li>
+            <li key={index}>
               <div className={styles.container}>
                 <img src={slide.url} />
               </div>
@@ -38,13 +39,17 @@ export default function Slider({ slides }: Props): ReactElement {
         <>
           <div className={styles.arrows}>
             {slides.map((slide, index) => {
-              return <label htmlFor={`slides_${index + 1}`}></label>;
+              return (
+                <label key={index} htmlFor={`slides_${index + 1}`}></label>
+              );
             })}
           </div>
           <div className={styles.navigation}>
             <div>
               {slides.map((slide, index) => {
-                return <label htmlFor={`slides_${index + 1}`}></label>;
+                return (
+                  <label key={index} htmlFor={`slides_${index + 1}`}></label>
+                );
               })}
             </div>
           </div>
@@ -53,83 +58,3 @@ export default function Slider({ slides }: Props): ReactElement {
     </div>
   );
 }
-
-/*
-<div className={styles["slider-wrapper"]}>
-        <div className={styles.slider}>
-          <input
-            type="radio"
-            name="slider"
-            className={styles.trigger}
-            id="one"
-            defaultChecked
-          />
-          <div className={styles.slide}>
-            <figure className={styles["slide-figure"]}>
-              <img
-                className={styles["slide-img"]}
-                src="/img/screenshots/dmenu.png"
-              />
-              <figcaption className={styles["slide-caption"]}>
-                <p>dmenu</p>
-              </figcaption>
-            </figure>
-          </div>
-          <input
-            type="radio"
-            name="slider"
-            className={styles.trigger}
-            id="two"
-          />
-          <div className={styles.slide}>
-            <figure className={styles["slide-figure"]}>
-              <img
-                className={styles["slide-img"]}
-                src="/img/screenshots/gtk.png"
-              />
-              <figcaption className={styles["slide-caption"]}>
-                <p>gtk</p>
-              </figcaption>
-            </figure>
-          </div>
-          <input
-            type="radio"
-            name="slider"
-            className={styles.trigger}
-            id="three"
-          />
-          <div className={styles.slide}>
-            <figure className={styles["slide-figure"]}>
-              <img
-                className={styles["slide-img"]}
-                src="/img/screenshots/duckduckgo.png"
-              />
-              <figcaption className={styles["slide-caption"]}>
-                <p>duckduckgo</p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-        <ul className={styles["slider-nav"]}>
-          <li className={styles["slider-nav__item"]}>
-            <label
-              className={styles["slider-nav__label"]}
-              htmlFor="one"
-            ></label>
-          </li>
-          <li className={styles["slider-nav__item"]}>
-            <label
-              className={styles["slider-nav__label"]}
-              htmlFor="two"
-            ></label>
-          </li>
-          <li className={styles["slider-nav__item"]}>
-            <label
-              className={styles["slider-nav__label"]}
-              htmlFor="three"
-            ></label>
-          </li>
-        </ul>
-      </div>
-
-*/
