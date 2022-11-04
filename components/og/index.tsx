4451dbed57c8e3fa6c9a1c2d763fa804959a979a
gabo <gabriel.gaboardi13@gmail.com>
2022-11-04 18:15:17 -0300

    updated og style, removed old components

--- components/og/index.tsx
+++ components/og/index.tsx
@@ -1,90 +1,76 @@
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row" as "row",
+    background: "linear-gradient(180deg,#19002e, #240041 70%)",
+    minHeight: "100vh",
+  },
+  imageContainer: {
+    display: "flex",
+    width: "30vw",
+    overflow: "hidden",
+  },
+  description: {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "40vw",
+    minWidth: "500px",
+    height: "100vh",
+    boxShadow: "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
+    background: "linear-gradient(180deg,#900048,#19002e 70%)",
+  },
+  logo: {
+    marginBottom: "40px",
+    width: "200px",
+    height: "200px",
+  },
+  heading: {
+    margin: 0,
+    lineHeight: "55.2px",
+    fontSize: "48px",
+    textAlign: "center" as "center",
+    color: "#ff0e82",
+  },
+  subheading: {
+    margin: "30px auto",
+    lineHeight: "40.8px",
+    fontSize: "27.2px",
+    color: "#c79bff",
+    maxWidth: "75%",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center" as "center",
+  },
+};
+
 export default function Index() {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        background: "linear-gradient(180deg,#19002e, #240041 70%)",
-        minHeight: "100vh",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          width: "30vw",
-          overflow: "hidden",
-        }}
-      >
+    <div style={styles.container}>
+      <div style={styles.imageContainer}>
         <img
-          style={{
-            height: "100%",
-          }}
+          style={{ height: "100%" }}
           src="https://wildberries.style/ports/vscode/screenshots/vscode-darker.png"
         />
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "40vw",
-          minWidth: "500px",
-          height: "100vh",
-          boxShadow:
-            "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
-          background: "linear-gradient(180deg,#900048,#19002e 70%)",
-        }}
-      >
+      <div style={styles.description}>
         <img
-          style={{
-            marginBottom: "40px",
-            width: "200px",
-            height: "200px",
-          }}
+          style={styles.logo}
           src="https://wildberries.style/img/ui/wb_logo.svg"
           alt="Wildberries"
         />
-        <h1
-          style={{
-            margin: 0,
-            lineHeight: "55.2px",
-            fontSize: "48px",
-            textAlign: "center",
-            color: "#ff0e82",
-          }}
-        >
-          Wildberries
-        </h1>
-        <p
-          style={{
-            margin: "30px auto",
-            lineHeight: "40.8px",
-            fontSize: "27.2px",
-            color: "#c79bff",
-            maxWidth: "75%",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
+        <h1 style={styles.heading}>Wildberries</h1>
+        <p style={styles.subheading}>
           An experimental dark theme for people who love purple
         </p>
       </div>
-      <div
-        style={{
-          display: "flex",
-          width: "30vw",
-          overflow: "hidden",
-        }}
-      >
+      <div style={styles.imageContainer}>
         <img
-          style={{
-            height: "100%",
-          }}
+          style={{ height: "100%" }}
           src="https://wildberries.style/ports/dwm/screenshots/dwm.png"
         />
       </div>
-    </main>
+    </div>
   );
 }
--- components/og/port.tsx
+++ components/og/port.tsx
@@ -4,92 +4,84 @@ interface Props {
   image: string | null;
 }
 
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row" as "row",
+    background: "linear-gradient(180deg,#19002e, #240041 70%)",
+    minHeight: "100vh",
+  },
+  description: {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minWidth: "50vw",
+    height: "100vh",
+    boxShadow: "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
+    background: "linear-gradient(180deg,#900048,#19002e 70%)",
+  },
+  logo: {
+    marginBottom: "40px",
+    width: "200px",
+    height: "200px",
+  },
+  heading: {
+    margin: 0,
+    lineHeight: "64.4px",
+    fontSize: "56px",
+    textAlign: "center" as "center",
+    color: "#ff0e82",
+  },
+  subheading: {
+    margin: "30px auto",
+    lineHeight: "48px",
+    fontSize: "32px",
+    color: "#c79bff",
+    maxWidth: "75%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  highlight: {
+    backgroundColor: "#c79bff",
+    color: "#19002e",
+    borderRadius: "5px",
+    padding: "5px 10px",
+    whiteSpace: "pre" as "pre",
+    marginLeft: "20px",
+  },
+  imageContainer: {
+    display: "flex",
+    margin: "auto",
+    width: "50vw",
+  },
+};
+
 export default function Port({ port, title, image }: Props) {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        background: "linear-gradient(180deg,#19002e, #240041 70%)",
-        minHeight: "100vh",
-      }}
-    >
+    <div style={styles.container}>
       <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: `${image ? "50vw" : "100vw"}`,
-          minWidth: "50vw",
-          height: "100vh",
-          boxShadow:
-            "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
-          background: "linear-gradient(180deg,#900048,#19002e 70%)",
-        }}
+        style={{ ...styles.description, width: `${image ? "50vw" : "100vw"}` }}
       >
         <img
-          style={{
-            marginBottom: "40px",
-            width: "200px",
-            height: "200px",
-          }}
+          style={styles.logo}
           src="https://wildberries.style/img/ui/wb_logo.svg"
           alt="Wildberries"
         />
-        <h1
-          style={{
-            margin: 0,
-            lineHeight: "64.4px",
-            fontSize: "56px",
-            textAlign: "center",
-            color: "#ff0e82",
-          }}
-        >
-          Wildberries
-        </h1>
-        <p
-          style={{
-            margin: "30px auto",
-            lineHeight: "48px",
-            fontSize: "32px",
-            color: "#c79bff",
-            maxWidth: "75%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+        <h1 style={styles.heading}>Wildberries</h1>
+        <p style={styles.subheading}>
           {`theme for `}
-          <span
-            style={{
-              backgroundColor: "#c79bff",
-              color: "#19002e",
-              borderRadius: "5px",
-              padding: "5px 10px",
-              whiteSpace: "pre",
-              marginLeft: "20px",
-            }}
-          >
-            {title}
-          </span>
+          <span style={styles.highlight}>{title}</span>
         </p>
       </div>
       {image && (
-        <div
-          style={{
-            display: "flex",
-            margin: "auto",
-            width: "50vw",
-          }}
-        >
+        <div style={styles.imageContainer}>
           <img
-            style={{
-              width: "100%",
-            }}
+            style={{ width: "100%" }}
             src={`https://wildberries.style/ports/${port}/screenshots/${image}`}
           />
         </div>
       )}
-    </main>
+    </div>
   );
 }
