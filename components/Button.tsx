c135a69b3d65541e01c1e0289d39affba56143e2
gabo <gabriel.gaboardi13@gmail.com>
2023-11-25 19:56:01 -0300

    refactored button to tw

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,24 @@
-import React, { ReactElement } from "react";
-import Link from "next/link";
-import styles from "../styles/Button.module.css";
+import React, { ReactElement } from 'react';
+import Link from 'next/link';
 
 export type Props = {
   href: string;
   children: string;
-  variant?: "primary" | "secondary";
+  variant?: 'primary' | 'secondary';
   icon?: string;
 };
 
-export default function Button({
-  href,
-  children,
-  variant = "primary",
-  icon,
-}: Props): ReactElement {
+const variants = {
+  primary: 'bg-cherry text-acid-green hover:bg-acid-green hover:text-cherry ',
+  secondary: 'bg-light-purple text-darker-purple hover:bg-darker-purple hover:text-light-purple ',
+};
+
+export default function Button({ href, children, variant = 'primary', icon }: Props): ReactElement {
   return (
     <Link href={href}>
-      <a className={styles[variant]}>
+      <a
+        className={`${variants[variant]} flex items-center justify-center gap-3 rounded-xl p-5 text-lg transition-all ease-out`}
+      >
         {icon && <img alt={icon} src={`/icons/${icon}.svg`} />}
         {children}
       </a>
