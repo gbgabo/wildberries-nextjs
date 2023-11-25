88dbf7dbe3f88ee0f709b06a0604a23df2774426
gabo <gabriel.gaboardi13@gmail.com>
2023-11-25 20:19:46 -0300

    updated code snippet css to tw

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,8 +1,7 @@
-import React, { ReactElement, ReactNode, useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { prism } from "../styles/highlight";
-import styles from "../styles/Code.module.css";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import React, { ReactElement, ReactNode, useState } from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { prism } from '../styles/highlight';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 interface Props {
   children: ReactNode;
@@ -10,19 +9,15 @@ interface Props {
   style?: any;
 }
 
-export default function Code({
-  children,
-  className = "",
-  style = prism,
-}: Props): ReactElement {
+export default function Code({ children, className = '', style = prism }: Props): ReactElement {
   const [copied, setCopied] = useState(false);
-  const code = String(children).replace(/\n$/, "");
+  const code = String(children).replace(/\n$/, '');
 
   const codeLanguage = /language-(\w+)/.exec(className);
 
   return (
-    <div className={styles.codeContainer}>
-      <div className={styles.copyButton}>
+    <div className="relative">
+      <div className="absolute bottom-4 right-4 rounded-md">
         <CopyToClipboard
           text={code}
           onCopy={() => {
@@ -33,16 +28,20 @@ export default function Code({
           }}
         >
           <img
-            className={copied ? styles.checkIcon : styles.copyIcon}
-            src={copied ? "/icons/done.svg" : "/icons/content_copy.svg"}
+            style={
+              copied
+                ? {
+                    filter: 'invert(83%) sepia(95%) saturate(4281%) hue-rotate(81deg) brightness(99%) contrast(106%)',
+                  }
+                : {
+                    filter: 'invert(28%) sepia(91%) saturate(7000%) hue-rotate(322deg) brightness(103%) contrast(101%)',
+                  }
+            }
+            src={copied ? '/icons/done.svg' : '/icons/content_copy.svg'}
           />
         </CopyToClipboard>
       </div>
-      <SyntaxHighlighter
-        language={codeLanguage ? codeLanguage[1] : "text"}
-        style={style}
-        PreTag="div"
-      >
+      <SyntaxHighlighter language={codeLanguage ? codeLanguage[1] : 'text'} style={style} PreTag="div">
         {code}
       </SyntaxHighlighter>
     </div>
