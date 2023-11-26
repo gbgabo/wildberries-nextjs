7b36a9c56d4b06e94efc3a40b695b730e6d8e1d3
gabo <gabriel.gaboardi13@gmail.com>
2023-11-26 20:23:55 -0300

    added support to variants

--- components/BioCard.tsx
+++ components/BioCard.tsx
@@ -3,12 +3,24 @@ interface Props {
   bottom?: string;
   image?: string;
   href: string;
+  variant?: 'primary' | 'secondary';
 }
 
-export default function BioCard({ top, bottom, image, href }: Props) {
+const variants = {
+  primary: {
+    text: 'text-acid-green',
+    button: 'bg-cherry text-acid-green hover:bg-acid-green hover:text-cherry',
+  },
+  secondary: {
+    text: 'text-light-purple',
+    button: 'bg-light-purple text-darker-purple hover:bg-darker-purple hover:text-light-purple',
+  },
+};
+
+export default function BioCard({ top, bottom, image, href, variant = 'secondary' }: Props) {
   return (
     <a className="grid w-52 grid-cols-4 grid-rows-2 content-center items-center justify-center gap-x-2" href={href}>
-      <p className="col-span-4 py-2">{top}</p>
+      <p className={`col-span-4 py-2 ${variants[variant].text}`}>{top}</p>
       <div className="w-8 place-self-center">
         {image ? (
           <img alt="external link" src={image} />
@@ -22,7 +34,7 @@ export default function BioCard({ top, bottom, image, href }: Props) {
           />
         )}
       </div>
-      <p className="berry col-span-3">{bottom}</p>
+      <p className={`col-span-3 whitespace-pre rounded py-1 ${variants[variant].button}`}>{bottom}</p>
     </a>
   );
 }
