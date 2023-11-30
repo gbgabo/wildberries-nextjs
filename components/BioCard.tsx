c38cc9ad4cdff8a9546efa036b4adbec2a7a776f
gabo <gabriel.gaboardi13@gmail.com>
2023-11-30 19:25:04 -0300

    updated biocard icon

--- components/BioCard.tsx
+++ components/BioCard.tsx
@@ -1,3 +1,5 @@
+import { Icon } from '@iconify/react';
+
 interface Props {
   top?: string;
   bottom?: string;
@@ -9,29 +11,26 @@ interface Props {
 const variants = {
   primary: {
     text: 'text-acid-green',
-    button: 'bg-cherry text-acid-green hover:bg-acid-green hover:text-cherry',
+    button: 'bg-cherry text-acid-green group-hover:bg-acid-green group-hover:text-cherry',
   },
   secondary: {
     text: 'text-light-purple',
-    button: 'bg-light-purple text-darker-purple hover:bg-darker-purple hover:text-light-purple',
+    button: 'bg-light-purple text-darker-purple group-hover:bg-darker-purple group-hover:text-acid-green',
   },
 };
 
 export default function BioCard({ top, bottom, image, href, variant = 'secondary' }: Props) {
   return (
-    <a className="grid w-52 grid-cols-4 grid-rows-2 content-center items-center justify-center gap-x-2" href={href}>
-      <p className={`col-span-4 py-2 ${variants[variant].text}`}>{top}</p>
+    <a
+      className="group grid w-52 grid-cols-4 grid-rows-2 content-center items-center justify-center gap-x-2"
+      href={href}
+    >
+      <p className={`col-span-4 py-2 transition-all ease-out ${variants[variant].text}`}>{top}</p>
       <div className="w-8 place-self-center">
         {image ? (
           <img alt="external link" src={image} />
         ) : (
-          <img
-            style={{
-              filter: 'invert(83%) sepia(95%) saturate(4281%) hue-rotate(81deg) brightness(99%) contrast(106%)',
-            }}
-            alt="external link"
-            src="/icons/open_in_new.svg"
-          />
+          <Icon icon="material-symbols:open-in-new" height="100%" width="auto" />
         )}
       </div>
       <p className={`col-span-3 whitespace-pre rounded py-1 transition-all ease-out ${variants[variant].button}`}>
