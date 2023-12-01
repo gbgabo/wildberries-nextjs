d5ebe71fb03ad8f8aa743317158381f2990a11ad
gabo <gabriel.gaboardi13@gmail.com>
2023-12-01 13:59:45 -0300

    updated image path

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,12 +16,7 @@ export default function Footer({ variant = 'primary' }: Props): ReactElement {
       className={`${variants[variant]} flex flex-col items-center justify-around gap-7 bg-darker-purple pb-56 pt-20 text-center text-acid-green md:flex-row`}
     >
       <BioCard top="Check the project on" bottom="Github" href="https://github.com/gbgabo/wildberries" />
-      <BioCard
-        top="Made with ♥ by"
-        bottom="@gbgabo"
-        image="/icons/profile_alpha.svg"
-        href="https://github.com/gbgabo"
-      />
+      <BioCard top="Made with ♥ by" bottom="@gbgabo" image="/img/profile_alpha.svg" href="https://github.com/gbgabo" />
     </footer>
   );
 }
